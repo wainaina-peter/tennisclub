92ac0692f9a0c7c6a6d31e94427002c4952a10c7
Peter Wainaina <peterwainaina@tovutigroup.com>
2024-01-02 15:56:34 +0300

    first commit of project

---
+++ members/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, slug):
+    mymember = Member.objects.get(slug=slug)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+
+def testing(request):
+    mydata = Member.objects.all().values()
+    template = loader.get_template('template.html')
+    context = {
+        'mymembers': mydata,
+    }
+    return HttpResponse(template.render(context, request))
